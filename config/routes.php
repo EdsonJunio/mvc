c48f9bdd40c698bb41be8bb55be87262a611d43c
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2024-07-08 11:42:53 -0300

    mvc

--- config/routes.php
+++ config/routes.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Alura\Mvc\Controller\DeleteVideoController;
 use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\jsonVideoListController;
 use Alura\Mvc\Controller\LoginController;
 use Alura\Mvc\Controller\LoginFormController;
 use Alura\Mvc\Controller\LogoutController;
+use Alura\Mvc\Controller\newJsonVideoController;
 use Alura\Mvc\Controller\NewVideoController;
 use Alura\Mvc\Controller\VideoFormController;
 use Alura\Mvc\Controller\VideoListController;
@@ -20,5 +22,7 @@ return [
     'GET|/remover-video' => DeleteVideoController::class,
     'GET|/login' => LoginFormController::class,
     'POST|/login' => LoginController::class,
-    'GET|/logout' => LogoutController::class
+    'GET|/logout' => LogoutController::class,
+    'GET|/videos' => jsonVideoListController::class,
+    'POST|/videos' => NewJsonVideoController::class
 ];
